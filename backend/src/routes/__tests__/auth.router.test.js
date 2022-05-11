eaa31dc37889b74c5e9b6b9bfc3c9a5cfe28984b
celso patiri <celso@pop-os.localdomain>
2022-05-11 11:44:59 -0300

    tests: added signin/signup success cases tests

---
+++ backend/src/routes/__tests__/auth.router.test.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+import request from "supertest";
+import { jestExpect as expect } from "@jest/expect";
+import app from "../../app.js";
+import { User } from "../../models/user.model.js";
+import bcrypt from "bcrypt";
+
+const MONGO_URI = process.env.MONGO_URI;
+
+describe("auth routes tests", () => {
+    beforeAll(() => {
+        mongoose.connect(MONGO_URI).then(async () => {
+            await User.create({
+                name: "testUser",
+                email: "test@test.com",
+                password: bcrypt.hashSync("1234", 10),
+            });
+        });
+    });
+
+    afterAll(async () => {
+        await User.deleteOne({ email: "test@test.com" });
+        await mongoose.connection.close();
+    });
+
+    const mockSignIn = {
+        email: "test@test.com",
+        password: "1234",
+    };
+
+    const mockSignUp = {
+        name: "testuser",
+        email: "test2@test.com",
+        password: "1234",
+    };
+
+    test("Sucessfull signin", async () => {
+        await request(app)
+            .post("/auth/signin")
+            .send(mockSignIn)
+            .expect(201)
+            .then((res) => {
+                const response = JSON.parse(res.text);
+                expect(response.userId).not.toBeFalsy();
+                expect(response.email).not.toBeFalsy();
+                expect(response.name).not.toBeFalsy();
+                expect(response.token).not.toBeFalsy();
+            });
+    });
+
+    test("Sucessfull signup", async () => {
+        await request(app)
+            .post("/auth/signup")
+            .send(mockSignUp)
+            .expect(201)
+            .then((res) => {
+                const response = JSON.parse(res.text);
+                expect(response.userId).not.toBeFalsy();
+                expect(response.email).not.toBeFalsy();
+                expect(response.name).not.toBeFalsy();
+                expect(response.token).not.toBeFalsy();
+            });
+    });
+});
