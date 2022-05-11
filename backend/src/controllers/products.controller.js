73c419478aa4905c25ddcffc87dad3884dcb475c
celso patiri <celso@pop-os.localdomain>
2022-05-11 19:16:31 -0300

    feat: products routes and controlllers

--- backend/src/controllers/products.controller.js
+++ backend/src/controllers/products.controller.js
@@ -1,4 +1,5 @@
 import { Product } from "../models/product.model.js";
+import { Category } from "../models/category.model.js";
 
 export const getProducts = async (req, res) => {
     const limit = req.query.limit || 10;
@@ -11,14 +12,33 @@ export const getProducts = async (req, res) => {
     }
 };
 
-export const getAllCategories = (req, res) => {
-    //TODO
+export const getAllCategories = async (_req, res) => {
+    try {
+        const categories = await Category.find({});
+        res.status(200).send(categories);
+    } catch (err) {
+        res.status(500).send({ error: err });
+    }
 };
 
-export const getProductsByCategory = (req, res) => {
-    //TODO
+export const getProductsByCategory = async (req, res) => {
+    const category = req.params.category;
+    const limit = req.query.limit || 10;
+    const skip = req.query.skip || 0;
+    try {
+        const products = await Product.find({ category }, {}, { skip, limit });
+        res.status(200).send(products);
+    } catch (err) {
+        res.status(500).send({ error: err });
+    }
 };
 
-export const getProductById = (req, res) => {
-    //TODO
+export const getProductById = async (req, res) => {
+    const productId = req.params.productId;
+    try {
+        const product = await Product.find({ _id: productId });
+        res.status(200).send(product);
+    } catch (err) {
+        res.status(500).send({ error: err });
+    }
 };
--- backend/src/routes/products.router.js
+++ backend/src/routes/products.router.js
@@ -11,7 +11,6 @@ const router = Router();
 router.get("/", getProducts);
 router.get("/categories", getAllCategories);
 router.get("/:productId", getProductById);
-//tem que pensar como as categorias vao ser classificadas e validadas
 router.get("/category/:category", getProductsByCategory);
 
 export default router;
