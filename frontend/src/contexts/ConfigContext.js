8b60932609c2437e88f5850af87233ef9b098b0d
celso patiri <celso@pop-os.localdomain>
2022-05-16 13:07:20 -0300

    add: dotenv -> REACT_APP_API_URL env variable

--- frontend/src/contexts/ConfigContext.js
+++ frontend/src/contexts/ConfigContext.js
@@ -1,18 +1,17 @@
-import { React, useState, createContext } from 'react';
+import { React, useState, createContext } from "react";
 
 export const ConfigContext = createContext();
 
 export function ConfigContextProvider({ children }) {
-
-    const apiLink = "http://localhost:5000";
+    const apiLink = process.env.REACT_APP_API_URL;
 
     return (
         <ConfigContext.Provider
             value={{
-                apiLink
+                apiLink,
             }}
         >
             {children}
         </ConfigContext.Provider>
-    )
+    );
 }
